140ee006fd3385b447af4fe44039d32f8627e798
Kevin Velickovic <kevin.velickovic@gmail.com>
2014-10-02 19:43:41 +0200

    Fixed header

--- js/plugins/map.js
+++ js/plugins/map.js
@@ -1,5 +1,5 @@
 /*
- * JSLoader - A simple JavaScript plugin architecture
+ * freepano - WebGL panorama viewer
  *
  * Copyright (c) 2014 FOXEL SA - http://foxel.ch
  * Please read <http://foxel.ch/license> for more information.
