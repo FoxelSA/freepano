36196994c60bcfdf1622b7d6a89c7385137bb8d4
Kevin Velickovic <kevin.velickovic@gmail.com>
2014-10-02 12:16:25 +0200

    Added selected marker highlighting

--- js/jquery.freepano.map.js
+++ js/jquery.freepano.map.js
@@ -132,18 +132,46 @@ $.extend(true, Map.prototype, {
             popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
         });
 
+        // Create highlighted eyesis icon
+        var eyesisIcon_Highlighted = L.icon({
+            iconUrl:     'img/eyesis_icon_highlight.png', // Icon image file path
+            shadowUrl:   'img/eyesis_shadow.png', // Shadow image file path
+
+            iconSize:     iconSize, // size of the icon
+            shadowSize:   shadowSize, // size of the shadow
+
+            iconAnchor:   iconAnchor, // point of the icon which will correspond to marker's location
+            shadowAnchor: shadowAnchor,  // the same for the shadow
+
+            popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+        });
+
         // Markers array
         var Markers = [];
+
+        // Retrive actual panorama name
+        var Pano = $("#pano").data("pano");
 
         // Iterate over panoramas and create markers
         $.each(panoramas, function( index, value ) {
 
+            // Determine if the marker is highlighted
+            var Icon = eyesisIcon;
+
+            if ( Pano.list.currentImage == index )
+                Icon = eyesisIcon_Highlighted;
+
             // Create marker
-            var Marker = L.marker([value.coords.lat, value.coords.lon], {icon: eyesisIcon})
+            var Marker = L.marker([value.coords.lat, value.coords.lon], {icon: Icon})
             .on('click', function(e) {
 
-                // Retrive actual panorama name
-                var Pano = $("#pano").data("pano");
+                // Reset all markers to default icon
+                $.each(Markers, function( index, value ) {
+                    value.setIcon(eyesisIcon);
+                });
+
+                // Set highlighted icon for marker
+                Marker.setIcon(eyesisIcon_Highlighted);
 
                 // Check if panorama has changed, then change it
                 if(e.target.panorama.index != Pano.list.currentImage)
