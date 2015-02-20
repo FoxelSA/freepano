358ed2bb24fe1026b73ec969ffa3895120d3fd1f
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2015-02-20 17:15:47 +0100

    add eventDispatcher documentation and license

--- js/eventDispatcher.js
+++ js/eventDispatcher.js
@@ -1,3 +1,73 @@
+/*
+ * freepano - WebGL panorama viewer
+ *
+ * Copyright (c) 2015 FOXEL SA - http://foxel.ch
+ * Please read <http://foxel.ch/license> for more information.
+ *
+ *
+ * Author(s):
+ *
+ *      Luc Deschenaux <l.deschenaux@foxel.ch>
+ *
+ *
+ * This file is part of the FOXEL project <http://foxel.ch>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional Terms:
+ *
+ *      You are required to preserve legal notices and author attributions in
+ *      that material or in the Appropriate Legal Notices displayed by works
+ *      containing it.
+ *
+ *      You are required to attribute the work as explained in the "Usage and
+ *      Attribution" section of <http://foxel.ch/license>.
+ */
+
+/*
+ *  Usage example: 
+ *
+ *  1. Setup the event dispatcher for the object prototype from which
+ *  you want to trigger events, eg:
+ *
+ *        setupEventDispatcher(Panorama.prototype);
+ *
+ *  Events can now be triggered with the dispatch() method, eg:
+ *
+ *       p=new Panorama({...}); p.dispatch('myevent',...);
+ *
+ *
+ *  2. Request dispatching events to another object prototype, eg:
+ *
+ *        Panorama.prototype.dispatchEventsTo(MyModule.prototype);
+ *
+ *  Now when an event is triggered with dispatch,
+ *  for each subscriber (in order of addition),
+ *  the "on_panorama_<event>" method will be called, if any.
+ *  And finally the method Panorama.on<event>() for the "source" prototype
+ *
+ *  If any handler return value is strictly equal to false, then
+ *  event propagation will be stopped.
+ *
+ *  3. Define handlers for required events in in MyModule.prototype,
+ *     eg "preinit", "ready", "update", "render" or "dispose",
+ *     (in the case of Panorama events)
+ *
+ *        MyModule.prototype.on_panorama_preinit: function myModule_on_panorama_preinit() {}
+ *
+ */
 
 var eventDispatcherDebug=1;
 
