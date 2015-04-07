30c4a38734cf6a7740ff6d7883789e1f8fa633bd
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2015-04-07 17:01:00 +0200

    pyramid.sh: better single level handling

--- bin/pyramid.sh
+++ bin/pyramid.sh
@@ -74,31 +74,42 @@ f=$2
   height=$(echo $resolution | cut -f 2 -d x)
   [ -z "$height" ] && exit
 
-  # get output resolution (closest superior multiple of tilesize)
+  # get the highest output resolution (closest superior multiple of tilesize)
   # and make directories
   newwidth=$tilesize
   level=-1
-  levelok=0
   while [ $newwidth -lt $width ] ; do
     level=$(expr $level + 1)
-    mkdir -p $base/$tilesize/$level || exit
-    [ -f $base/$tilesize/$level/done ] && levelok=$(expr $levelok + 1)
+
+    # if a single level is requested, dont bother with other directories
+    if [ -z "$LEVEL" -o -n "$LEVEL" -a "$LEVEL" = "$level" ] ;  then
+      mkdir -p $base/$tilesize/$level || exit
+    fi
+
     newwidth=$(expr $newwidth \* 2)
+
   done
 
   bottom=$level
   echo pyramid levels: $(expr $bottom + 1)
 
-  # resize original file if width not multiple of tilesize
+  # resize original file if width not multiple of tilesize,
   tempfile=
   halfw=$(expr $newwidth / 2)
   if [ $width -ne $newwidth -o $height -ne $halfw ] ; then
     width=$newwidth
     height=$halfw
-    tempfile=/tmp/tmp.$(basename $f).tiff
-    convert $f -resize ${width}x$height -quality 100 $tempfile
-    fref=$f
-    f=$tempfile
+
+    # if a single level != bottom level is requested,
+    # or bottom level is already done,
+    # then just use the new width and height set above
+    if [ -z "$LEVEL" -o -n "$LEVEL" -a "$LEVEL" = "$bottom" ] && [ ! -f $base/$tilesize/$bottom/done ]  ; then 
+      tempfile=/tmp/tmp.$(basename $f).tiff
+      convert $f -resize ${width}x$height -quality 100 $tempfile
+      fref=$f
+      f=$tempfile
+    fi
+
   fi
 
   echo -n output resolution: $width\x$height
@@ -107,16 +118,24 @@ f=$2
   level=$bottom
   curwidth=$width
 
+  echo
+  echo
+
   while [ $level -ge 0 ] ; do
-    echo
-    echo
-    echo -n "level: $level - ${curwidth}x$(expr $curwidth / 2) "
 
     # skip level if flagged as 'done', or is not the specific level requested
     if [ -f $base/$tilesize/$level/done ] || [ -n "$LEVEL" -a "$level" != "$LEVEL" ]; then
-      echo -n "- skipped"
+
+      if [ -z "$LEVEL" -o -n "$LEVEL" -a -f $base/$tilesize/$level/done ]  ; then
+        echo -n "level: $level - ${curwidth}x$(expr $curwidth / 2) "
+        echo -n "- skipped"
+        echo
+        echo
+      fi
 
     else
+
+      echo -n "level: $level - ${curwidth}x$(expr $curwidth / 2) "
 
       if [ $level -eq $bottom ] ; then # higher resolution level
 
@@ -174,6 +193,9 @@ f=$2
 
       # flag level as done
       touch $base/$tilesize/$level/done
+
+      echo
+      echo
 
     fi
 
